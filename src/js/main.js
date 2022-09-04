1a561e3c36ddcffaab94ba1b10478ba8ff27de74
Shakhawat <shakhawat589@gmail.com>
2022-09-05 02:38:20 +0600

    Install Bootstrap with webpack, sass

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Import our custom CSS
+import "../scss/styles.scss";
+
+// require("bootstrap-icons/font/bootstrap-icons.css");
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
